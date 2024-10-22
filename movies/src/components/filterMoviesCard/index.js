20fe16611e295dbb1de5dab19a7811c5bff1a10f
FinnBennett1 <122983686+FinnBennett1@users.noreply.github.com>
2024-10-22 21:46:38 +0100

    Change site header

--- movies/src/components/filterMoviesCard/index.js
+++ movies/src/components/filterMoviesCard/index.js
@@ -29,6 +29,7 @@ export default function FilterMoviesCard(props) {
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+  
   const handleChange = (e, type, value) => {
     e.preventDefault()
     props.onUserInput(type, value)   
